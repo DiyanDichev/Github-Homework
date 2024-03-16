5430008549325e1518a00e75ac4b31c500241cba
ayaks90 <145563673+ayaks90@users.noreply.github.com>
2024-03-17 01:20:38 +0200

    update

---
+++ Java OOP/Day05_Inheritance/P03RandomArrayList/Main.java
@@ -0,0 +1,8 @@
+package P03RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+        randomArrayList.getRandomElement();
+    }
+}
