4379b96cecf24ce782177c992bf0e9c39df78f9f
ayaks90 <145563673+ayaks90@users.noreply.github.com>
2024-03-18 01:20:56 +0200

    update

---
+++ Java OOP/Day11_SOLID/P03LiskovSubstitution/_01Square/Shape.java
@@ -0,0 +1,5 @@
+package Day11_SOLID.P03LiskovSubstitution._01Square;
+
+public abstract class Shape {
+    public abstract double getArea();
+}
---
+++ Java OOP/Day11_SOLID/P03LiskovSubstitution/_01Square/Square.java
@@ -0,0 +1,24 @@
+package Day11_SOLID.P03LiskovSubstitution._01Square;
+
+public class Square extends Rectangle {
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
+}
