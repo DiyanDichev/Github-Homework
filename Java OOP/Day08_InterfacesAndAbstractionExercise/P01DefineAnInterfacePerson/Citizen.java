9886c2f1f0641a24dde77051aecdec0d3beaabe8
ayaks90 <145563673+ayaks90@users.noreply.github.com>
2024-03-17 14:01:55 +0200

    update

---
+++ Java OOP/Day08_InterfacesAndAbstractionExercise/P01DefineAnInterfacePerson/Citizen.java
@@ -0,0 +1,21 @@
+package Day08_InterfacesAndAbstractionExercise.P01DefineAnInterfacePerson;
+
+public class Citizen implements Person{
+    private String name;
+    private int age;
+
+    public Citizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
---
+++ Java OOP/Day08_InterfacesAndAbstractionExercise/P01DefineAnInterfacePerson/Person.java
@@ -0,0 +1,6 @@
+package Day08_InterfacesAndAbstractionExercise.P01DefineAnInterfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
