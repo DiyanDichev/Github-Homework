48099e8b60f03e1c049c774c5bfbf04b3e7beae3
ayaks90 <145563673+ayaks90@users.noreply.github.com>
2024-03-19 17:44:48 +0200

    update

---
+++ Java OOP/Day16_UnitTestingExercise/P06_TirePressureMonitoringSystem/AlarmTest.java
@@ -0,0 +1,32 @@
+package P06_TirePressureMonitoringSystem;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class AlarmTest {
+    @Test
+    public void testAlarmWithLowerValue(){
+        Sensor sensor = Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(15.3);
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+    @Test
+    public void testAlarmWithHigherValue(){
+        Sensor sensor = Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(22.4);
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+    @Test
+    public void testAlarmWithNormalValue(){
+        Sensor sensor = Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(19.3);
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertFalse(alarm.getAlarmOn());
+    }
+}
